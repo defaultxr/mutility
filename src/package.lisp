4b4a67eb8bcedd56235d99d56d0b45afa0232d17
modula t. worm <defaultxr@gmail.com>
2021-11-07 21:51:53 -0600

    export using uninterned symbols to avoid polluting namespaces

--- src/package.lisp
+++ src/package.lisp
@@ -4,78 +4,78 @@
   (:use #:cl
         #:alexandria)
   (:export
-   :a
-   :fn
-   :cut
-   :with-access
-   :no-dictionary-entry
-   :no-dictionary-entry-entry
-   :no-dictionary-entry-dictionary-name
-   :no-dictionary-entry-dictionary
-   :define-dictionary
+   #:a
+   #:fn
+   #:cut
+   #:with-access
+   #:no-dictionary-entry
+   #:no-dictionary-entry-entry
+   #:no-dictionary-entry-dictionary-name
+   #:no-dictionary-entry-dictionary
+   #:define-dictionary
 
-   :dprint
+   #:dprint
 
-   :keys
+   #:keys
 
-   :upcase-intern
-   :my-intern
-   :reintern
-   :un-intern
+   #:upcase-intern
+   #:my-intern
+   #:reintern
+   #:un-intern
 
-   :friendly-string
-   :friendly-symbol
+   #:friendly-string
+   #:friendly-symbol
 
-   :concat
-   :output
-   :vowel-char-p
-   :string-designator-p
-   :split-string
-   :replace-all
-   :parse-boolean
-   :friendly-ratio-string
-   :friendly-duration-string
-   :pretty-print-tree
+   #:concat
+   #:output
+   #:vowel-char-p
+   #:string-designator-p
+   #:split-string
+   #:replace-all
+   #:parse-boolean
+   #:friendly-ratio-string
+   #:friendly-duration-string
+   #:pretty-print-tree
 
-   :wrap
-   :fold
-   :floor-by
-   :ceiling-by
-   :round-by
-   :round-by-direction
+   #:wrap
+   #:fold
+   #:floor-by
+   #:ceiling-by
+   #:round-by
+   #:round-by-direction
 
-   :list-length-upto
-   :length-upto
-   :list-length>=
-   :list-length>
-   :nth-wrap
-   :elt-wrap
-   :find-if*
-   :find-any
-   :most
-   :flatten-1
-   :subseq*
-   ;; :repeat ;; conflicts with iterate
-   :split-sequence
-   :left-trim
-   :affixnew
-   :insert-if
-   :insert-sorted
+   #:length-upto
+   #:list-length-upto
+   #:list-length>=
+   #:list-length>
+   #:nth-wrap
+   #:elt-wrap
+   #:find-if*
+   #:find-any
+   #:most
+   #:flatten-1
+   #:subseq*
+   ;; #:repeat ;; conflicts with iterate
+   #:split-sequence
+   #:left-trim
+   #:affixnew
+   #:insert-if
+   #:insert-sorted
 
-   :random-coin
-   :random-range
-   :exponential-random-range
-   :random-gauss
+   #:random-coin
+   #:random-range
+   #:exponential-random-range
+   #:random-gauss
 
-   :save-hash-table
-   :restore-hash-table
+   #:save-hash-table
+   #:restore-hash-table
 
-   :current-seconds
-   :function-arglist
-   :lisp-connections
+   #:current-seconds
+   #:function-arglist
+   #:lisp-connections
    
-   :pathname-designator
-   :pathname-designator-p
-   :open-url
-   :generate-temporary-file-name
-   :locate-dominating-file))
+   #:pathname-designator
+   #:pathname-designator-p
+   #:open-url
+   #:generate-temporary-file-name
+   #:locate-dominating-file))
